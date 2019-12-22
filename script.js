a8499ee27d5138160221ce26e552bb9859e730fd
balgopal123 <n.balgopalpatra@gmail.com>
2019-12-22 23:40:57 +0530

    Firebase Database Linked

--- script.js
+++ script.js
@@ -1,4 +1,27 @@
-function locationfun(){
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBGdT1NqdlmpGM7nt5cIKI2ypE-OTQnqz8",
+    authDomain: "duckhawk-1699a.firebaseapp.com",
+    databaseURL: "https://duckhawk-1699a.firebaseio.com",
+    projectId: "duckhawk-1699a",
+    storageBucket: "duckhawk-1699a.appspot.com",
+    messagingSenderId: "310324939369",
+    appId: "1:310324939369:web:c290e9fdf3a88b4bb930f8",
+    measurementId: "G-Y3CR0HNHNW"
+  };
+
+  
+firebase.initializeApp(firebaseConfig);  
+
+
+var database = firebase.database();
+console.log(database);
+
+
+
+
+
+  function locationfun(){
     // document.getElementsByClassName('.onlocation').style["transform"] =  " translateX(0px)";
     document.getElementById('locationbox').style.width = "500px";
     // document.getElementsByClassName('.onlocation').style["top"] =  "7vh";
@@ -9,3 +32,5 @@ function clearlocationSearchbox(){
     document.getElementById('locationbox').style.width = "0px";
     document.getElementById('locationin').value = "";
 }
+
+
